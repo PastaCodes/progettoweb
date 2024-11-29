1cb790eec94d37f8ceb5edcd47fd6334448ee00f
Fuwuffy <palazzini21@gmail.com>
2024-11-29 14:30:42 +0100

    Add php product class

---
+++ Product.php
@@ -0,0 +1,11 @@
+<?php
+class Product {
+    public string $name;
+    public float $price;
+
+    function __construct(string $name, float $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+}
+?>
