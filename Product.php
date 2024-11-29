c62df6603bb2a938617b4c6fb268ecd376602715
Fuwuffy <palazzini21@gmail.com>
2024-11-29 15:02:33 +0100

    Add url property to product

--- Product.php
+++ Product.php
@@ -2,10 +2,12 @@
 class Product {
     public string $name;
     public float $price;
+    public string $imageUrl;
 
-    function __construct(string $name, float $price) {
+    function __construct(string $name, float $price, string $imageUrl) {
         $this->name = $name;
         $this->price = $price;
+        $this->imageUrl = $imageUrl;
     }
 }
 ?>
