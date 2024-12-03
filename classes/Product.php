4d9a22efdce21678900335025add4ae59f2015c9
Fuwuffy <palazzini21@gmail.com>
2024-12-03 16:48:33 +0100

    Add radio button to change product variant

--- classes/Product.php
+++ classes/Product.php
@@ -1,12 +1,14 @@
 <?php
 class Product {
-    public string $name;
+    public string $code;
+    public string $display_name;
     public float $priceMin;
     public float $priceMax;
     public ?string $image;
 
-    function __construct(string $name, float $priceMin, float $priceMax, ?string $image = null) {
-        $this->name = $name;
+    function __construct(string $code, string $display_name, float $priceMin, float $priceMax, ?string $image = null) {
+        $this->code = $code;
+        $this->display_name = $display_name;
         $this->priceMin = $priceMin;
         $this->priceMax = $priceMax;
         $this->image = $image;
--- components/products.php
+++ components/products.php
@@ -5,9 +5,9 @@
     $products = [];
     $result = $db->query('SELECT * FROM product JOIN price_range ON product = code WHERE NOT bundle_only');
     while ($row = $result->fetch_assoc()) {
-        $products[] = new Product($row['display_name'], $row['price_min'], $row['price_max']);
+        $products[] = new Product($row['code'], $row['display_name'], $row['price_min'], $row['price_max']);
     }
-
+    
     function format_price(float $price) : string {
         return number_format($price, 2, decimal_separator: ",", thousands_separator: ".");
     }
@@ -35,7 +35,23 @@
 <?php endif ?>
                     </section>
                     <section>
-                        <span><?= $product->name ?></span>
+                        <fieldset>
+<?php
+    $colors = [];
+    $result = $db->query('SELECT `color` FROM `product_variant` WHERE `product` = \'' . $product->code . '\'');
+    while ($row = $result->fetch_assoc()) {
+        $colors[] = $row['color'];
+    }
+    $checked = true;
+?>
+<?php foreach ($colors as $color): ?>
+                            <input type="radio" name="<?= $product->code ?>_color"<?= $checked ? ' checked="checked"' : '' ?> />
+<?php $checked = false; ?>
+<?php endforeach ?>
+                        </fieldset>
+                    </section>
+                    <section>
+                        <span><?= $product->display_name ?></span>
                         <small><?= format_price_range($product) ?></small>
                     </section>
                 </article>
