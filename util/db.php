6fafeaa7adb847f9e47b0c941880b1981745c43f
Fuwuffy <palazzini21@gmail.com>
2024-12-03 12:09:48 +0100

    Fix database case sensitive in php

--- util/db.php
+++ util/db.php
@@ -3,7 +3,7 @@ if (SETTINGS['hosted-locally']) {
     $servername = 'localhost';
     $username = 'root';
     $password = '';
-    $dbname = 'IsiFitGems';
+    $dbname = 'isifitgems';
 } else
     die('Only local hosting is currently supported.');
 $db = new mysqli($servername, $username, $password, $dbname);
