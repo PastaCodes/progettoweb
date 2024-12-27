9ac3d2c1e80d82df0ffc227ee4b8fbe47a41fa0c
Fuwuffy <palazzini21@gmail.com>
2024-12-27 16:31:35 +0100

    Add header to navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,13 +1,15 @@
-<nav>
-    <ul>
-        <li>
-            <a href="#">Homepage</a>
-        </li>
-        <li>
-            <a href="#">Products</a>
-        </li>
-        <li>
-            <a href="#">Carrello</a>
-        </li>
-    </ul>
-</nav>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Homepage</a>
+            </li>
+            <li>
+                <a href="#">Products</a>
+            </li>
+            <li>
+                <a href="#">Carrello</a>
+            </li>
+        </ul>
+    </nav>
+</header>
