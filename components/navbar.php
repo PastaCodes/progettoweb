21efed5fef3b4ad8218a241e015b97c4dd3add54
Fuwuffy <palazzini21@gmail.com>
2024-12-27 14:57:17 +0100

    Add basic navbar component

---
+++ components/navbar.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li>
+            <a href="">Homepage</a>
+        </li>
+        <li>
+            <a href="">Products</a>
+        </li>
+        <li>
+            <a href="">Carrello</a>
+        </li>
+    </ul>
+</nav>
