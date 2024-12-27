b6f087f3de8bd83bd03555a778b248db914a3bab
Fuwuffy <palazzini21@gmail.com>
2024-12-27 15:01:33 +0100

    Add empty footer component

--- components/products.php
+++ components/products.php
@@ -27,9 +27,7 @@ while ($products_row = $products_result->fetch_assoc()) {
     $products[] = new Product($product_code, $products_row['display_name'], $products_row['price_min'], $products_row['price_max'], $variants, $first_thumbnail);
 }
 ?>
-        <?php
-            require 'components/navbar.php';
-        ?>
+        <?php require 'components/navbar.php'; ?>
         <template id="no-thumbnail">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <use href="assets/ban-solid.svg#root"></use>
@@ -65,3 +63,4 @@ while ($products_row = $products_result->fetch_assoc()) {
 <?php endforeach ?>
             </section>
         </main>
+        <?php require 'components/footer.php'; ?>
