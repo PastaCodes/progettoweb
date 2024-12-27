c43342e17730d681bb08f148bb903acc36d662e6
Fuwuffy <palazzini21@gmail.com>
2024-12-27 15:53:19 +0100

    Change theme switcher code to lambda function

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -2,18 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add dummy button
     // TODO: improve button code and position stuff 
     const btn = document.createElement("button");
-    btn.innerHTML = "Change theme";
     document.querySelector("body").appendChild(btn);
     // Get document tag
     const docElt = document.documentElement;
-    // Set current theme on the page
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    docElt.setAttribute('data-theme', currentTheme);
-    btn.addEventListener('click', () => {
-        // Switch theme
-        const newTheme = docElt.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
-        docElt.setAttribute('data-theme', newTheme);
+    // Function to switch the theme of the page
+    const switchTheme = (themeStr) => {
+        // Switch button text
+        btn.innerHTML = themeStr === 'dark' ? '🌙' : '☀️';
+        // Switch page color theme
+        docElt.setAttribute('data-theme', themeStr);
         // Set theme preference to new theme
-        localStorage.setItem('theme', newTheme);
+        localStorage.setItem('theme', themeStr);
+    };
+    switchTheme(localStorage.getItem('theme') || 'light');
+    // Switch theme on button press
+    btn.addEventListener('click', () => {
+        switchTheme(docElt.getAttribute('data-theme') === 'light' ? 'dark' : 'light');
     });
 });
