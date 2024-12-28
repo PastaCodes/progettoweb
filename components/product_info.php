95c8c07144f43379608ec5d4f3663ab6f18baea0
Fuwuffy <palazzini21@gmail.com>
2024-12-28 14:57:42 +0100

    Base product info page

--- components/product_info.php
+++ components/product_info.php
@@ -1,3 +1,44 @@
-<main>
+<?php
+require __DIR__ . '/../' . 'util/db.php';
+require __DIR__ . '/../' . 'classes/Product.php';
+require __DIR__ . '/../' . 'classes/ProductVariant.php';
+require __DIR__ . '/../' . 'util/format.php';
+require __DIR__ . '/../' . 'util/files.php';
 
-</main>
+$product = null;
+$product_result = $db->query('select code_name, display_name, price_min, price_max from product_base join price_range on product = code_name where standalone = true and code_name = chain_bracelet');
+while ($products_row = $products_result->fetch_assoc()) {
+    $variants_result = $db->query('select code_suffix, display_name, color from product_variant join product_info on product = base and variant = code_suffix where base = \'' . $products_row['code_name'] . '\' order by ordinal asc');
+    $variants = [];
+    $first_thumbnail = null;
+    $product_code = $products_row['code_name'];
+    if ($variants_result->num_rows > 0) {
+        while ($variants_row = $variants_result->fetch_assoc()) {
+            $variant_code = $product_code . '_' . $variants_row['code_suffix'];
+            $thumbnail_file = get_thumbnail_if_exists($variant_code);
+            $variants[] = new ProductVariant($variants_row['display_name'], $variants_row['color'], $thumbnail_file);
+            if (count($variants) == 1)
+                $first_thumbnail = $thumbnail_file;
+            else if ($thumbnail_file)
+                $prefetch[] = $thumbnail_file;
+        }
+    } else
+        $first_thumbnail = get_thumbnail_if_exists($product_code);
+    $product = new Product($product_code, $products_row['display_name'], $products_row['price_min'], $products_row['price_max'], $variants, $first_thumbnail);
+}
+?>
+        <main>
+        <?php if ($product): ?>
+            <section>
+                <h1>Product not found</h1>
+            </section>
+        <?php else: ?>
+            <section>
+                <img src="immagine.png"></img>
+            </section>
+            <section>
+                <h1>Titolo</h1>
+                <p>Decrizione</p>
+            </section>
+        <?php endif ?>
+        </main>
