19e164d993da5c63d8b94ba996d4414fe1f3ceb5
PastaCodes <marco.j.buda@gmail.com>
2024-12-30 14:46:06 +0100

    Add some comments to script.js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+// Wait for the style to be rendered
 window.addEventListener('load', () => {
+    // Retrieve the elements to be used when no thumbnail is available
     const noThumbnailTemplate = document.getElementById('no-thumbnail');
     document.querySelectorAll('article').forEach(article => {
         const radiosSection = article.querySelector('section:nth-child(2):not(:last-child)');
         if (!radiosSection)
-            return;
+            return; // Skip to next article
         const thumbnailSection = article.querySelector('section:nth-child(1)');
         const displayThumbnail = (activeRadio) => {
             let img = thumbnailSection.querySelector('img');
             const thumbnail = activeRadio.getAttribute('data-thumbnail');
-            if (thumbnail && !img)
+            if (thumbnail && !img) {
+                // Replace the 'no thumbnail' elements with a new img
                 thumbnailSection.replaceChildren(img = document.createElement('img'));
+            }
             if (thumbnail) {
+                // Reuse the already present img to avoid flashes
                 img.src = thumbnail;
                 img.loading = 'eager';
-            } else if (img)
+            } else if (img) {
+                // Replace the img with the 'no thumbnail' elements
                 thumbnailSection.innerHTML = noThumbnailTemplate.innerHTML;
+            }
         }
         const children = Array.from(radiosSection.children);
+        // When hovering over a radio button, the associated thumbnail is displayed
+        // Otherwise the one associated with the checked button is displayed
         children.forEach(radio => {
             radio.addEventListener('mouseover', () => {
                 if (!radio.checked)
@@ -29,13 +38,17 @@ window.addEventListener('load', () => {
             // For silly little browsers that do not support attr styling
             radio.style.setProperty('--radio-color', radio.getAttribute('data-color'));
         });
+        // Up to 5 radio buttons can be displayed neatly without needing the scrolling functionality
         if (children.length > 5) {
+            // When clicking on a section assign a moveHandler and an upHandler (if it doesn't have one already)
             radiosSection.addEventListener('pointerdown', ev => {
                 if (!radiosSection.moveHandler) {
                     radiosSection.moveHandler = ev => {
                         if (ev.pointerId === radiosSection.moveHandler.id) {
                             radiosSection.scrollBy(radiosSection.moveHandler.lastX - ev.clientX, 0);
                             radiosSection.moveHandler.lastX = ev.clientX;
+                            // Any movement of more than 10 px is considered intentional,
+                            // which means no click action should occur
                             if (Math.abs(radiosSection.moveHandler.lastX - radiosSection.moveHandler.initialX) > 10)
                                 radiosSection.moveHandler.target = null;
                         }
@@ -60,10 +73,14 @@ window.addEventListener('load', () => {
                     window.addEventListener('pointerup', upHandler);
                 }
             });
+            // Scrolls to have the selected radio button in the center (or close to it)
             const updateScroll = () => {
                 const radio = children[radiosSection.selectedIndex];
                 const radioStyle = window.getComputedStyle(radio);
                 const radiosSectionStyle = window.getComputedStyle(radiosSection);
+                // To avoid having too many radio buttons hidden, and also remain consistent with the '5 policy',
+                // clamp the index so that, when clicking the first and second buttons, the third gets centered,
+                // and, similarly, when clicking the last and second-to-last buttons, the third-to-last gets centered
                 const clampedIndex = Math.min(Math.max(radiosSection.selectedIndex, 2), children.length - 3);
                 const offset = (clampedIndex - 2) * (parseFloat(radioStyle.width) + parseFloat(radiosSectionStyle.gap));
                 radiosSection.scrollTo({
@@ -80,12 +97,15 @@ window.addEventListener('load', () => {
                         radiosSection.selectedIndex = index;
                         updateScroll();
                     } else {
+                        // Ignore/disable all events triggered directly by the browser
                         ev.preventDefault();
                         ev.stopPropagation();
                     }
                 });
             });
-            window.addEventListener('resize', () => updateScroll());
+            // The scroll position might need to be updated when zooming or resizing
+            // This is likely because of the font size breakpoints set by pico
+            window.addEventListener('resize', updateScroll);
         }
     });
 });
