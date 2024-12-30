00126112398b63340b592f30a905a14031f83536
PastaCodes <marco.j.buda@gmail.com>
2024-12-30 16:17:18 +0100

    Add deadzone to radio buttons

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ window.addEventListener('load', () => {
                 thumbnailSection.innerHTML = noThumbnailTemplate.innerHTML;
             }
         }
-        const children = Array.from(radiosSection.children);
+        article.radios = Array.from(radiosSection.children);
         // When hovering over a radio button, the associated thumbnail is displayed
         // Otherwise the one associated with the checked button is displayed
-        children.forEach(radio => {
+        article.radios.forEach(radio => {
             radio.addEventListener('mouseover', () => {
                 if (!radio.checked)
                     displayThumbnail(radio);
@@ -39,18 +39,21 @@ window.addEventListener('load', () => {
             radio.style.setProperty('--radio-color', radio.getAttribute('data-color'));
         });
         // Up to 5 radio buttons can be displayed neatly without needing the scrolling functionality
-        if (children.length > 5) {
+        if (article.radios.length > 5) {
             // When clicking on a section assign a moveHandler and an upHandler (if it doesn't have one already)
             radiosSection.addEventListener('pointerdown', ev => {
                 if (!radiosSection.moveHandler) {
+                    radiosSection.dragged = null;
                     radiosSection.moveHandler = ev => {
                         if (ev.pointerId === radiosSection.moveHandler.id) {
                             radiosSection.scrollBy(radiosSection.moveHandler.lastX - ev.clientX, 0);
                             radiosSection.moveHandler.lastX = ev.clientX;
                             // Any movement of more than 10 px is considered intentional,
                             // which means no click action should occur
-                            if (Math.abs(radiosSection.moveHandler.lastX - radiosSection.moveHandler.initialX) > 10)
+                            if (Math.abs(radiosSection.moveHandler.lastX - radiosSection.moveHandler.initialX) > 10) {
                                 radiosSection.moveHandler.target = null;
+                                radiosSection.dragged = ev.pointerId;
+                            }
                         }
                     };
                     radiosSection.moveHandler.id = ev.pointerId;
@@ -75,13 +78,13 @@ window.addEventListener('load', () => {
             });
             // Scrolls to have the selected radio button in the center (or close to it)
             const updateScroll = () => {
-                const radio = children[radiosSection.selectedIndex];
+                const radio = article.radios[radiosSection.selectedIndex];
                 const radioStyle = window.getComputedStyle(radio);
                 const radiosSectionStyle = window.getComputedStyle(radiosSection);
                 // To avoid having too many radio buttons hidden, and also remain consistent with the '5 policy',
                 // clamp the index so that, when clicking the first and second buttons, the third gets centered,
                 // and, similarly, when clicking the last and second-to-last buttons, the third-to-last gets centered
-                const clampedIndex = Math.min(Math.max(radiosSection.selectedIndex, 2), children.length - 3);
+                const clampedIndex = Math.min(Math.max(radiosSection.selectedIndex, 2), article.radios.length - 3);
                 const offset = (clampedIndex - 2) * (parseFloat(radioStyle.width) + parseFloat(radiosSectionStyle.gap));
                 radiosSection.scrollTo({
                     top: 0,
@@ -91,7 +94,7 @@ window.addEventListener('load', () => {
             };
             radiosSection.selectedIndex = 0;
             updateScroll();
-            children.forEach((radio, index) => {
+            article.radios.forEach((radio, index) => {
                 radio.addEventListener('click', ev => {
                     if (ev.isProgrammatic) {
                         radiosSection.selectedIndex = index;
@@ -107,5 +110,40 @@ window.addEventListener('load', () => {
             // This is likely because of the font size breakpoints set by pico
             window.addEventListener('resize', updateScroll);
         }
+        article.deadzonePointers = new Set();
+        article.addEventListener('pointermove', ev => {
+            const deadzone = 6 / window.devicePixelRatio;
+            let firstRect = article.radios[0].getBoundingClientRect();
+            let diameter = firstRect.width;
+            let inDeadzone = ev.clientY >= firstRect.y - deadzone && ev.clientY <= firstRect.y + diameter + deadzone; // Rough y check
+            if (inDeadzone) {
+                let deadRadius = diameter / 2 + deadzone;
+                let deadRadius2 = deadRadius * deadRadius;
+                inDeadzone = article.radios.some(radio => {
+                    let rect = radio.getBoundingClientRect();
+                    // Rough x check
+                    if (ev.clientX < rect.x - deadzone || ev.clientX > rect.x + diameter + deadzone)
+                        return false;
+                    let dx = ev.clientX - (rect.x + diameter / 2);
+                    let dy = ev.clientY - (rect.y + diameter / 2);
+                    return dx * dx + dy * dy <= deadRadius2;
+                });
+            }
+            if (inDeadzone) {
+                article.style.cursor = 'default';
+                article.deadzonePointers.add(ev.pointerId);
+            } else {
+                article.style.cursor = 'pointer';
+                article.deadzonePointers.delete(ev.pointerId);
+            }
+        });
+        article.addEventListener('click', ev => {
+            // If this pointer was dragged on the radiosSection do not register the click
+            if (ev.target === radiosSection && radiosSection.dragged === ev.pointerId)
+                return;
+            if (article.deadzonePointers.has(ev.pointerId))
+                return;
+            console.log('CLICK'); // TODO
+        });
     });
 });
