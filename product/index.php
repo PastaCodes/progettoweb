a2b490e94b8e1c0f21dae3c974cb5cc4a91daec0
Fuwuffy <palazzini21@gmail.com>
2025-01-10 17:05:26 +0100

    Fix script issue with product page

--- product/index.php
+++ product/index.php
@@ -5,6 +5,6 @@ $page->body = 'components/product_info.php';
 $page->allow_indexing = false;
 $page->has_navbar = true;
 $page->has_feet = true;
-$page->scripts = [Script::external('scripts/cart.js', 'module')];
+$page->scripts[] = Script::external('scripts/cart.js', 'module');
 require_once '../components/base.php';
 ?>
