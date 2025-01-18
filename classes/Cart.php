dcba9f0a4297a529c39d8111418d15dbaf490cc0
Fuwuffy <palazzini21@gmail.com>
2025-01-18 20:40:14 +0100

    Make cart use new database system

--- classes/Cart.php
+++ classes/Cart.php
@@ -15,30 +15,34 @@ class Cart {
     }
 
     public function fetch_details() {
-        global $db;
-        $details_result = $db->query('
-            select b.code_name base_code_name, v.code_suffix variant_code_suffix,
-                    b.display_name base_display_name, v.display_name variant_display_name, price
-	            from product_base b left join product_variant v on v.base = b.code_name
-                    join product_info on product = b.code_name and (v.code_suffix is null or variant = v.code_suffix)
-                where ' . implode(' or ', array_map(function($entry) {
-                    return '(b.code_name = \'' . $entry->product->base->code_name . '\' and ' .
-                        ($entry->product->variant === null ?
-                            'v.code_suffix is null' :
-                            'v.code_suffix = \'' . $entry->product->variant->code_suffix . '\''
-                        ) . ')';
-                }, $this->entries))
+        global $database;
+        $details_result = $database->find(
+            table: 'product_base',
+            joins: [
+                [
+                    'type' => 'INNER',
+                    'table' => 'product_info',
+                    'on' => 'product = code_name',
+                ],
+                [
+                    'type' => 'LEFT',
+                    'table' => 'product_variant',
+                    'on' => 'base = code_name',
+                ]
+            ],
+            filters: ['product_base.code_name' => array_map(fn($entry): string => $entry->product->base->code_name, $this->entries)],
+            options: ['distinct' => true]
         );
         $details = [];
-        while ($details_row = $details_result->fetch_assoc()) {
-            Product::from($details_row['base_code_name'], $details_row['variant_code_suffix'])->array_set($details, $details_row);
+        foreach ($details_result as $details_row) {
+            Product::from($details_row['code_name'], $details_row['code_suffix'])->array_set($details, $details_row);
         }
         foreach ($this->entries as $entry) {
             $details_row = $entry->product->array_get($details);
-            $entry->product->base->display_name = $details_row['base_display_name'];
+            $entry->product->base->display_name = $details_row['product_base.display_name'];
             $entry->product->price = $details_row['price'];
             if ($entry->product->variant !== null) {
-                $entry->product->variant->display_name = $details_row['variant_display_name'];
+                $entry->product->variant->display_name = $details_row['product_variant.display_name'];
             }
         }
     }
