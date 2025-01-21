d96aaa5086af5d3a5ff68ae357ba0899ad35a013
Fuwuffy <palazzini21@gmail.com>
2025-01-21 20:52:02 +0100

    Slight reordering of php base file stuff

--- util/setup.php
+++ util/setup.php
@@ -12,12 +12,12 @@ const SETTINGS = [
 define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'] . (SETTINGS['hosted-locally'] ? '/IsiFitGems/' : '/'));
 $page = new Page();
 $page->stylesheets = ['style/pico.classless.min.css', 'style/global.css'];
-$page->scripts = [Script::external('scripts/global.js', 'module')];
-$page->prefetch = [];
 add_file_if_exists(get_directory() . 'style.css', $page->stylesheets);
+$page->scripts = [Script::external('scripts/global.js', 'module')];
 // FIXME: Temporary fix
 if (file_exists_rel('./' . get_directory() . 'script.js')) {
     $page->scripts[] = Script::external(get_directory() . 'script.js', 'module');
 }
 // add_file_if_exists(get_directory() . 'script.js', $page->scripts, 'Script::external');
+$page->prefetch = [];
 ?>
