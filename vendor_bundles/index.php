b247918b4d32b6fc765d0778962aeb5c436d556e
Fuwuffy <palazzini21@gmail.com>
2025-01-23 17:03:35 +0100

    Add empty vendor pages

---
+++ vendor_bundles/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../util/setup.php';
+$page->title = 'Modify Bundles - IsiFitGems';
+$page->body = 'vendor_bundles/modify_bundles.php';
+$page->has_navbar = true;
+$page->has_feet = true;
+require_once '../components/base.php';
+?>
