d8093930c7af30468f5210c3ae086354a8f518b2
Fuwuffy <palazzini21@gmail.com>
2025-01-25 14:21:07 +0100

    Fix colorthemes vendor pages

--- vendor_bundles/index.php
+++ vendor_bundles/index.php
@@ -5,6 +5,6 @@ $page->body = 'vendor_bundles/modify_bundles.php';
 $page->has_navbar = true;
 $page->has_feet = true;
 $page->stylesheets[] = 'vendor_products/style.css';
-$page->scripts = [Script::external('scripts/table_collapsable.js', 'module')];
+$page->scripts[] = Script::external('scripts/table_collapsable.js', 'module');
 require_once '../components/base.php';
 ?>
--- vendor_products/index.php
+++ vendor_products/index.php
@@ -4,6 +4,6 @@ $page->title = 'Modify Products - IsiFitGems';
 $page->body = 'vendor_products/modify_products.php';
 $page->has_navbar = true;
 $page->has_feet = true;
-$page->scripts = [Script::external('scripts/table_collapsable.js', 'module')];
+$page->scripts[] = Script::external('scripts/table_collapsable.js', 'module');
 require_once '../components/base.php';
 ?>
