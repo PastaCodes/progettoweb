3e5317f18a4c00ad59005fec8549051bf0d729e4
PastaCodes <marco.j.buda@gmail.com>
2025-01-25 19:16:41 +0100

    Part2

--- orders/order_history.php
+++ orders/order_history.php
@@ -1,3 +1,10 @@
+<?php
+// Check if vendor
+if ($_SESSION['vendor'] ?? false) {
+    header('Location: ../shop');
+    exit();
+}
+?>
         <main>
             <h1>Your orders</h1>
         </main>
